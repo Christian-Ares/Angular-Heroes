a3b0d293f9aa6e676d2f464e821103279d368333
Christian Ares <c.ares@exatrek.de>
2024-05-27 12:20:49 +0200

    Models added

---
+++ src/app/mocks/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from '../models/heroe';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice', superpower: 'Beauty' },
+  { id: 13, name: 'Bombasto', superpower: 'Bombs' },
+  { id: 14, name: 'Celeritas', superpower: 'Speed' },
+  { id: 15, name: 'Magneta', superpower: 'Magnets' },
+  { id: 16, name: 'RubberMan', superpower: 'Elastic' },
+  { id: 17, name: 'Dynama', superpower: 'Telekinesis' },
+  { id: 18, name: 'Dr. IQ', superpower: 'Intelligence' },
+  { id: 19, name: 'Magma', superpower: 'Lava' },
+  { id: 20, name: 'Tornado', superpower: 'Wind' }
+];
